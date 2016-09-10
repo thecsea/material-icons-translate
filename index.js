9b57ba26760043a068ded69f45dcb24be483b1fb
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-09-10 10:01:00 +0200

    [structure] index and dependencies

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 /**
  * Created by claudio on 09/09/16.
  */
+var MaterialIconsTranslator = require('./src/MaterialIconsTranslator');
+
+/**
+ *
+ * @param content JSON or OBJECT
+ * @param options
+ * @returns Promise
+ */
+module.exports = function(content){
+    return new MaterialIconsTranslator(content).translate();
+}
