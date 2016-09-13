d1b00ed7fff04b2817b1b7d685a1006f0b88f706
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-09-13 13:04:56 +0200

    [elixir] elixir done

---
+++ elixir.js
@@ -0,0 +1,27 @@
+/**
+ * Created by claudio on 13/09/16.
+ */
+var Elixir = require('laravel-elixir');
+var Gulp = require('gulp');
+
+var Print = require('gulp-print');
+var Plumber = require('gulp-plumber');
+
+var MaterialIconsTranslator = require('./gulp');
+
+var Task = Elixir.Task;
+
+Elixir.extend('jsonImagesSaver', function (src, pipe, simple)
+{
+    new Task('jsonImagesSaver', function ()
+    {
+        return Gulp
+            .src(src)
+            .pipe(Print())
+            .pipe(Plumber())
+            .pipe(MaterialIconsTranslator(simple))
+            .pipe(pipe())
+            .pipe(Print());
+    }).watch(src);
+});
+//TODO notify
