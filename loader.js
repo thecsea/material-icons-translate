ce6b2f3d48d575644f3de8d6f1b7b2f55b8a1887
Claudio Cardinale <cardinale.claudio@gmail.com>
2017-10-22 16:10:45 +0200

    [loader] minor fix

--- loader.js
+++ loader.js
@@ -7,6 +7,7 @@ const MaterialIconsTranslator = require('./index');
 const loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
+  this.cacheable && this.cacheable();
   let options = loaderUtils.getOptions(this) || {};
   return MaterialIconsTranslator.simple(source, options.debug || false);
 };
